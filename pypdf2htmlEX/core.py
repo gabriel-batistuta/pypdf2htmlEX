e2eecb31da3bfd4439dac0f56ee60c494b2b92cc
gabriel <batistutag190@gmail.com>
2024-05-24 17:55:59 -0300

    alter name of modules folder to pypdf2htmlEX

--- pypdf2htmlEX/core.py
+++ pypdf2htmlEX/core.py
@@ -1,11 +1,11 @@
 import subprocess
 
 class PDF():
-    def __init__(self, pdf_file_path:str, drm=False):
+    def __init__(self, pdf_file_path:str, drm=None):
         self.file_path = pdf_file_path    
         self.drm = drm
 
-    def __make_pdf(self, args=None):
+    def __make_html(self, args=None):
 
         def add_extra_options(args, options):
             if args:
@@ -15,7 +15,7 @@ class PDF():
                 return options
 
         if self.drm is True:
-            options = ["pdf2htmlEX", f"{self.file_path}", "--no-drm", f"{self.drm}"]
+            options = ["pdf2htmlEX", f"{self.file_path}", "--no-drm", "1"]
             options = add_extra_options(args, options)
             subprocess.call(options)
         else:
@@ -23,10 +23,10 @@ class PDF():
             options = add_extra_options(args, options)
             subprocess.call(options)
 
-    def to_pdf(self, file_path=None):
+    def to_html(self, file_path=None):
         if file_path:
             args = ["-o", file_path]
-            self.__make_pdf(args)
+            self.__make_html(args)
         else:
-            self.__make_pdf()
+            self.__make_html()
         
